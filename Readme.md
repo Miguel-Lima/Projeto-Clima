e96fe1ce3ef9a11b1af8669ef1ca26d0936dd143
Miguel Lima <miguellimaoliver97@gmail.com>
2021-08-31 12:13:15 -0300

    Commit Final

--- Readme.md
+++ Readme.md
@@ -1,7 +1,3 @@
-Estrutura HTML 99% Finalizada;
-CSS 50% Finalizada;
-JS 100% Finalizado ; Aguardando apenas a liberação da API para a funcionalidade;
-Projeto em andamento;
-
-JS Adicionado;
-Aguardando Liberação da API;
+Estrutura HTML 100% Finalizada;
+CSS 100% Finalizada;
+JS 100% Finalizado ; 
--- projeto3/script.js
+++ projeto3/script.js
@@ -6,7 +6,7 @@ document.querySelector('.busca').addEventListener('submit', async (event)=> {
     if(input !== '') {
         showWarning('Carregando ...')
 
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=23f6a93687b37e89b4424a55a241d210&units=metric&lang=pt_br`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=23f6a93687b37e89b4424a55a241d210&units=metric&lang=pt_br`;
         
         let results = await fetch(url);
         let json = await results.json();
@@ -36,14 +36,15 @@ function showInfo(json) {
     showWarning('');
 
     document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
-    document.querySelector('tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
-    document.querySelector('ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp}, <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed}, <span>km/h</span>`;
 
     document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
 
     document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
 
     document.querySelector('.resultado').style.display = 'block';
+    
 }
 
 function clearInfo() {
